96625cf436b24a07e861da607f8089b0fc9cff7e
wjgin <woojin0913@naver.com>
2022-05-31 22:15:23 +0900

    feat: upload ch4 Ballot_Dapp

--- Ch.4/Ballot-Dapp/ballot-contract/contracts/Ballot.sol
+++ Ch.4/Ballot-Dapp/ballot-contract/contracts/Ballot.sol
@@ -1,34 +1,71 @@
-pragma solidity >= 0.4.22 <= 0.6.0;
-contract BallotV1 {
+pragma solidity >=0.4.22 <=0.6.0;
 
-    struct Voter {
- 
-		}
+contract Ballot {
 
-    struct Proposal {
-   
-		}
-
-    address chairperson;
-    mapping(address => Voter) voters;
-    Proposal[] proposals;
-
-    modifier onlyChair(){
-			require(msg.sender == chairperson);
-			_;
+    struct Voter {                     
+        uint weight;
+        bool voted;
+        uint vote;
+    }
+    struct Proposal {                  
+        uint voteCount;
     }
 
-		modifier validVoter() {
-			
-			require(voters[msg.sender].weight > 0, "Not a Resigtered Voter");
-			_;
-		}
+    address chairperson;
+    mapping(address => Voter) voters;  
+    Proposal[] proposals;
 
-    constructor(uint numProposals) public { }
+    
+       //modifiers
+   
+    modifier onlyChair() 
+     {require(msg.sender == chairperson);
+      _;
+     }
+     
+     modifier validVoter()
+    {
+        require(voters[msg.sender].weight > 0, "Not a Registered Voter");
+        _;
+    }
 
-    function register(address voter) public onlyChair { }
+    constructor (uint numProposals) public  {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 2; // weight 2 for testing purposes
+        //proposals.length = numProposals; -- before 0.6.0
+        for (uint prop = 0; prop < numProposals; prop ++)
+            proposals.push(Proposal(0));
+        
+    }
+    
+     
+    function register(address voter) public  onlyChair {
+        
+        voters[voter].weight = 1;
+        voters[voter].voted = false;
+    }
 
-    function vote(uint toProposal) public validVoter { }
+   
+    function vote(uint toProposal) public  validVoter{
+      
+        Voter memory sender = voters[msg.sender];
+        
+        require (!sender.voted); 
+        require (toProposal < proposals.length); 
+        
+        sender.voted = true;
+        sender.vote = toProposal;   
+        proposals[toProposal].voteCount += sender.weight;
+    }
 
-		function reqWinner() public view returns (uint winningProposal){ }
+    function reqWinner() public view returns (uint winningProposal) {
+       
+        uint winningVoteCount = 0;
+        for (uint prop = 0; prop < proposals.length; prop++) 
+            if (proposals[prop].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[prop].voteCount;
+                winningProposal = prop;
+            }
+       assert(winningVoteCount>=3);
+    }
 }
